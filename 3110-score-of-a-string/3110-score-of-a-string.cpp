6b895e8e75d235b3745bcbbece7f38b402bb0ad4
PRIYANSHU RAJ <146221357+Priyanshu-Raj81@users.noreply.github.com>
2026-07-14 18:38:12 +0530

    Time: 0 ms (100%), Space: 9 MB (31.8%) - LeetHub

---
+++ 3110-score-of-a-string/3110-score-of-a-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int score = 0;
+        int n = s.length()-1;
+        for(int i = 0; i < n; i++){
+            score += abs(s[i] - s[i+1]);
+
+        }
+        return score;
+    }
+};
