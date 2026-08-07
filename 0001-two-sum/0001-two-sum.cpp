eb462452776fade40644bb585df6fa28cb357d3d
PRIYANSHU RAJ <146221357+Priyanshu-Raj81@users.noreply.github.com>
2026-08-07 14:37:18 +0530

    Time: 40 ms (26.52%), Space: 14.1 MB (84.77%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,31 +1,42 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<pair<int,int>> arr;
-        for(int i=0;i<nums.size();i++){
-            arr.push_back({nums[i],i});
-        }
+        // vector<pair<int,int>> arr;
 
-        int start = 0;
-        int end = nums.size()-1;
-        sort(arr.begin(), arr.end());
+        // //store value with original index
+        // for(int i=0;i<nums.size();i++){
+        //     arr.push_back({nums[i],i});
+        // }
+
+        // int start = 0;
+        // int end = nums.size()-1;
+        // sort(arr.begin(), arr.end());
         
 
-        while(start < end){
-            int sum = arr[start].first + arr[end].first;
+        // while(start < end){
+        //     int sum = arr[start].first + arr[end].first;
 
-            if(sum == target){
-                return {arr[start].second, arr[end].second};
-            }
-            else if (sum < target){
-                start++;
-            }
-            else{
-                end--;
+        //     if(sum == target){
+        //         return {arr[start].second, arr[end].second};
+        //     }
+        //     else if (sum < target){
+        //         start++;
+        //     }
+        //     else{
+        //         end--;
+        //     }
+        // }
+
+        // return {};
+        int n = nums.size();
+        for(int i=0; i<n; i++){
+            for(int j= i+1; j<n; j++){
+                if(nums[i]+nums[j] == target){
+                    return {i,j};
+                }
             }
         }
-
-        return {};
+        return{};
                
     }
 };
